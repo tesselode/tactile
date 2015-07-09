fb4d15ed2a8c3882b8305e338878a09e914a3b4e
tesselode <aminnich3@gmail.com>
2015-07-09 00:57:28 -0400

    Binary axis detector + axes

--- main.lua
+++ main.lua
@@ -1,8 +1,15 @@
 function love.load()
   tactile = require 'tactile'
-  testAxis = tactile:addGamepadAxisDetector('leftx', 1)
+  
+  keyboardLeft  = tactile:addKeyboardButtonDetector('left')
+  keyboardRight = tactile:addKeyboardButtonDetector('right')
+  keyboardXAxis = tactile:addBinaryAxisDetector(keyboardLeft, keyboardRight)
+  gamepadXAxis  = tactile:addGamepadAxisDetector('leftx', 1)
+  
+  horizontal    = tactile:addAxis(keyboardXAxis, gamepadXAxis)
 end
 
 function love.update(dt)
-  print(testAxis())
+  tactile:update(dt)
+  print(horizontal.value)
 end
--- tactile.lua
+++ tactile.lua
@@ -18,6 +18,20 @@ function button:update()
   self.released = self.downPrev and not self.down
 end
 
+--axis class
+local axis = {}
+axis.__index = axis
+
+function axis:update()
+  self.value = 0
+  
+  for k, v in pairs(self.detectors) do
+    if v() and math.abs(v()) > self.deadzone then
+      self.value = v()
+    end
+  end
+end
+
 --main module
 local tactile = {}
 tactile.__index = tactile
@@ -30,6 +44,18 @@ function tactile:addKeyboardButtonDetector(key)
 end
 
 --axis detectors
+function tactile:addBinaryAxisDetector(negative, positive)
+  return function()
+    if negative() and not positive() then
+      return -1
+    elseif positive() and not negative() then
+      return 1
+    else
+      return false
+    end
+  end
+end
+
 function tactile:addGamepadAxisDetector(axis, gamepadNum)
   return function()
     if self.gamepads[gamepadNum] then
@@ -53,8 +79,22 @@ function tactile:addButton(...)
   return setmetatable(buttonInstance, button)
 end
 
+--axis constructor
+function tactile:addAxis(...)
+  local axisInstance = {
+    detectors = {...},
+    deadzone  = self.deadzone,
+    value     = 0
+  }
+  table.insert(self.axes, axisInstance)
+  return setmetatable(axisInstance, axis)
+end
+
 function tactile:update()
   for k, v in pairs(self.buttons) do
+    v:update()
+  end
+  for k, v in pairs(self.axes) do
     v:update()
   end
 end
@@ -63,7 +103,9 @@ end
 function tactile.new()
   local inputHandler = {
     gamepads = love.joystick.getJoysticks(),
-    buttons = {}
+    deadzone = 0.25,
+    buttons  = {},
+    axes     = {}
   }
   return setmetatable(inputHandler, tactile)
 end
