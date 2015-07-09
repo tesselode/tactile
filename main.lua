e6c2e479d73836446c7d321fcea5c70576df7008
tesselode <aminnich3@gmail.com>
2015-07-09 00:47:44 -0400

    Gamepad axis detector

--- main.lua
+++ main.lua
@@ -1,11 +1,8 @@
 function love.load()
   tactile = require 'tactile'
-  keyboardLeft  = tactile:addKeyboardButtonDetector('left')
-  keyboardRight = tactile:addKeyboardButtonDetector('right')
-  eitherLeftOrRight = tactile:addButton(keyboardLeft, keyboardRight)
+  testAxis = tactile:addGamepadAxisDetector('leftx', 1)
 end
 
 function love.update(dt)
-  tactile:update()
-  print(eitherLeftOrRight.down)
+  print(testAxis())
 end
--- tactile.lua
+++ tactile.lua
@@ -1,3 +1,4 @@
+--button class
 local button = {}
 button.__index = button
 
@@ -5,25 +6,41 @@ function button:update()
   self.downPrev = self.down
   self.down = false
   
+  --check whether any detectors are down
   for k, v in pairs(self.detectors) do
     if v() then
       self.down = true
     end
   end
   
+  --pressed and released states
   self.pressed  = self.down and not self.downPrev
   self.released = self.downPrev and not self.down
 end
 
+--main module
 local tactile = {}
 tactile.__index = tactile
 
+--button detectors
 function tactile:addKeyboardButtonDetector(key)
   return function()
     return love.keyboard.isDown(key)
   end
 end
 
+--axis detectors
+function tactile:addGamepadAxisDetector(axis, gamepadNum)
+  return function()
+    if self.gamepads[gamepadNum] then
+      return self.gamepads[gamepadNum]:getGamepadAxis(axis)
+    else
+      return false
+    end
+  end
+end
+
+--button constructor
 function tactile:addButton(...)
   local buttonInstance = {
     detectors = {...},
@@ -42,11 +59,14 @@ function tactile:update()
   end
 end
 
+--gives you a new input handler
 function tactile.new()
   local inputHandler = {
+    gamepads = love.joystick.getJoysticks(),
     buttons = {}
   }
   return setmetatable(inputHandler, tactile)
 end
 
+--return a default input handler
 return tactile.new()
