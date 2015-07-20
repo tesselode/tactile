e5163440ad63c0789d7eed4bbf5d83636643ff8c
tesselode <aminnich3@gmail.com>
2015-07-19 22:46:48 -0400

    Calling this v1.0.0

--- tactile.lua
+++ tactile.lua
@@ -1,3 +1,32 @@
+local tactile = {
+  _VERSION     = 'Tactile v1.0.0',
+  _DESCRIPTION = 'A simple and straightfoward input library for LOVE.',
+  _URL         = 'https://github.com/tesselode/tactile',
+  _LICENSE     = [[
+    The MIT License (MIT)
+
+    Copyright (c) 2015 Andrew Minnich
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+  ]]
+}
+
 local function removeByValue(t, value)
   for i = #t, 1, -1 do
     if t[i] == value then
@@ -63,7 +92,6 @@ function Axis:removeDetector(detector)
 end
 
 --main module
-local tactile = {}
 tactile.__index = tactile
 tactile.deadzone = .25
 tactile.gamepads = love.joystick.getJoysticks()
