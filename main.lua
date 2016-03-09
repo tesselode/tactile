2dc6832f371806a0988d3a3ec0c8c2be825f3898
Kingdaro <kingdaro@gmail.com>
2016-03-08 20:35:16 -0500

    add changes from rewrite

--- main.lua
+++ main.lua
@@ -1,131 +1,27 @@
-function ButtonDisplay(x, y, button)
-  local buttonDisplay = {}
-  buttonDisplay.x             = x
-  buttonDisplay.y             = y
-  buttonDisplay.button        = button
-  buttonDisplay.pressedAlpha  = 0
-  buttonDisplay.releasedAlpha = 0
+local tactile = require 'tactile'
 
-  function buttonDisplay:update (dt)
-    if self.button:pressed() then
-      self.pressedAlpha = 255
-    end
-    if self.button:released() then
-      self.releasedAlpha = 255
-    end
-
-    self.pressedAlpha = self.pressedAlpha - 1500 * dt
-    self.releasedAlpha = self.releasedAlpha - 1500 * dt
-    if self.pressedAlpha < 0 then
-      self.pressedAlpha = 0
-    end
-    if self.releasedAlpha < 0 then
-      self.releasedAlpha = 0
-    end
-  end
-
-  function buttonDisplay:draw()
-    if self.button.down then
-      love.graphics.setColor(79, 102, 66, 255)
-      love.graphics.rectangle('fill', self.x, self.y, 50, 50)
-    end
-
-    love.graphics.setColor(213, 194, 128, self.pressedAlpha)
-    love.graphics.rectangle('fill', self.x, self.y, 50, 50)
-
-    love.graphics.setColor(126, 179, 181, self.releasedAlpha)
-    love.graphics.rectangle('fill', self.x, self.y, 50, 50)
-
-    love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.rectangle('line', self.x, self.y, 50, 50)
-  end
-
-  return buttonDisplay
-end
-
-function love.load()
-  tactile = require 'tactile'
-
-  --basic detectors
-  keyboardLeft  = tactile.keys('left', 'a')
-  keyboardRight = tactile.keys('right', 'd')
-  keyboardUp    = tactile.keys('up', 'w')
-  keyboardDown  = tactile.keys('down', 's')
-  keyboardX     = tactile.keys('x')
-  gamepadA      = tactile.gamepadButtons(1, 'a')
-  gamepadXAxis  = tactile.gamepadAxis(1, 'leftx')
-  gamepadYAxis  = tactile.gamepadAxis(1, 'lefty')
-  mouseLeft     = tactile.mouseButtons(1)
-
-  --weird detectors that depend on other detectors
-  gamepadLeft   = tactile.thresholdButton(gamepadXAxis, -.5)
-  gamepadRight  = tactile.thresholdButton(gamepadXAxis, .5)
-  gamepadUp     = tactile.thresholdButton(gamepadYAxis, -.5)
-  gamepadDown   = tactile.thresholdButton(gamepadYAxis, .5)
-  keyboardXAxis = tactile.binaryAxis(keyboardLeft, keyboardRight)
-  keyboardYAxis = tactile.binaryAxis(keyboardUp, keyboardDown)
-
-  button = {}
-  button.left       = tactile.newButton(keyboardLeft, gamepadLeft)
-  button.right      = tactile.newButton(keyboardRight, gamepadRight)
-  button.up         = tactile.newButton(keyboardUp, gamepadUp)
-  button.down       = tactile.newButton(keyboardDown, gamepadDown)
-  button.primary    = tactile.newButton(keyboardX, gamepadA, mouseLeft)
-
-  axis = {}
-  axis.horizontal = tactile.newAxis(gamepadXAxis, keyboardXAxis)
-  axis.vertical   = tactile.newAxis(gamepadYAxis, keyboardYAxis)
-  axis.horizontal.deadzone = .25
-  axis.vertical.deadzone = .25
-
-  upButtonDisplay      = ButtonDisplay(50, 0, button.up)
-  leftButtonDisplay    = ButtonDisplay(0, 50, button.left)
-  downButtonDisplay    = ButtonDisplay(50, 100, button.down)
-  rightButtonDisplay   = ButtonDisplay(100, 50, button.right)
-  primaryButtonDisplay = ButtonDisplay(0, 0, button.primary)
-end
+local horizontal = tactile.newControl()
+horizontal:addButtonPair(
+  tactile.keys('left', 'a'),
+  tactile.keys('right', 'd')
+)
+horizontal:addAxisDetector(tactile.gamepadAxis(1, 'leftx'))
 
 function love.update(dt)
-  for k, v in pairs(button) do
-    v:update()
-  end
+  horizontal:update()
 
-  upButtonDisplay:update(dt)
-  leftButtonDisplay:update(dt)
-  downButtonDisplay:update(dt)
-  rightButtonDisplay:update(dt)
-  primaryButtonDisplay:update(dt)
+  if horizontal:pressed() then
+    print 'pressed'
+  end
+  if horizontal:released() then
+    print 'released'
+  end
+end
+
+function love.keypressed(key)
+  if key == 'escape' then love.event.quit() end
 end
 
 function love.draw()
-  love.graphics.push()
-  love.graphics.translate(120, 120)
-
-  love.graphics.setColor(255, 255, 255, 255)
-  love.graphics.rectangle('line', -100, -100, 200, 200)
-  love.graphics.rectangle('line', -.25 * 100, -.25 * 100, .25 * 200, .25 * 200)
-  love.graphics.circle('fill', axis.horizontal:getValue() * 100, axis.vertical:getValue() * 100, 5, 100)
-  love.graphics.printf('The square represents two axes. It can be operated by the left analog stick on joystick 1 or the arrow keys. The inner square is the deadzone (custom set to 25%).', -75, 120, 150, 'center')
-
-  love.graphics.pop()
-
-  love.graphics.push()
-  love.graphics.translate(350, 45)
-
-  upButtonDisplay:draw()
-  leftButtonDisplay:draw()
-  downButtonDisplay:draw()
-  rightButtonDisplay:draw()
-  love.graphics.printf('These are 4 directional buttons. They light up when held down, and flash when pressed or released. These can be operated by both the left analog stick on joystick 1 and the arrow keys. These particular ones are set with a threshold of 0.5.', 0, 220, 150, 'center')
-
-  love.graphics.pop()
-
-  love.graphics.push()
-  love.graphics.translate(650, 100)
-
-  primaryButtonDisplay:draw()
-  love.graphics.setColor(255, 255, 255, 255)
-  love.graphics.printf('This is a button that is activated by the X key, the A button on joystick 1, or the left mouse button.', -50, 220, 150, 'center')
-
-  love.graphics.pop()
+  love.graphics.print(tostring(horizontal:isDown()))
 end
--- tactile.lua
+++ tactile.lua
@@ -1,230 +1,144 @@
-local tactile = {
-  _VERSION     = 'Tactile v1.3',
-  _DESCRIPTION = 'A simple and straightfoward input library for LÖVE.',
-  _URL         = 'https://github.com/tesselode/tactile',
-  _LICENSE     = [[
-    The MIT License (MIT)
+local tactile = {}
 
-    Copyright (c) 2015 Andrew Minnich
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-  ]]
-}
-
-local function removeByValue(t, value)
-  for i = #t, 1, -1 do
-    if t[i] == value then
-      table.remove(t, i)
-      break
+local function any(t, f)
+  for i = 1, #t do
+    if f(t[i]) then
+      return true
     end
   end
+  return false
 end
 
-local function verify(identity, argnum, arg, argtype, expected)
-  if type(arg) ~= argtype then
-    error(("%s: argument %d should be a %s (got %s)"):format(identity, argnum, expected or argtype, type(arg)), 3)
+local function sign(x)
+  if x < 0 then
+    return -1
+  else
+    return 0
   end
 end
 
---button class
-local Button = {}
-Button.__index = Button
+local Control = {}
 
-function Button:update()
-  self.downPrev = self.down
-  self.down = false
+function Control:addAxisDetector(f)
+  table.insert(self._detectors, f)
+end
 
-  --check whether any detectors are down
-  for i = 1, #self.detectors do
-    if self.detectors[i]() then
-      self.down = true
-      break
+function Control:addPositiveButtonDetector(f)
+  table.insert(self._detectors, function()
+    if f() then
+      return 1
+    else
+      return 0
+    end
+  end)
+end
+
+function Control:addNegativeButtonDetector(f)
+  table.insert(self._detectors, function()
+    if f() then
+      return -1
+    else
+      return 0
+    end
+  end)
+end
+
+function Control:addButtonPair(negative, positive)
+  table.insert(self._detectors, function()
+    local n, p = negative(), positive()
+    if n and p then
+      return 0
+    elseif n then
+      return -1
+    elseif p then
+      return 1
+    else
+      return 0
+    end
+  end)
+end
+
+function Control:getValue()
+  for i = #self._detectors, 1, -1 do
+    local value = self._detectors[i]()
+    if math.abs(value) > self.deadzone then
+      return value
     end
   end
+  return 0
 end
 
-function Button:addDetector(detector)
-  table.insert(self.detectors, detector)
-end
-
-function Button:removeDetector(detector)
-  removeByValue(self.detectors, detector)
-end
-
-function Button:isDown() return self.down end
-function Button:pressed() return self.down and not self.downPrev end
-function Button:released() return self.downPrev and not self.down end
-
---axis class
-local Axis = {}
-Axis.__index = Axis
-
-function Axis:getValue(deadzone)
-  deadzone = deadzone or self.deadzone
-  self.value = 0
-
-  --check whether any detectors have a value greater than the deadzone
-  for i = 1, #self.detectors do
-    local value = self.detectors[i]()
-    if math.abs(value) > deadzone then
-      self.value = value
+function Control:isDown(dir)
+  if self:getValue() == 0 then
+    return false
+  end
+  if dir then
+    if sign(self:getValue()) ~= sign(dir) then
+      return false
     end
   end
-
-  return self.value
+  return true
 end
 
-function Axis:addDetector(detector)
-  table.insert(self.detectors, detector)
+function Control:pressed(dir)
+  if self._currentValue == 0 or self._previousValue ~= 0 then
+    return false
+  end
+  if dir then
+    if sign(self._currentValue) ~= sign(dir) then
+      return false
+    end
+  end
+  return true
 end
 
-function Axis:removeDetector(detector)
-  removeByValue(self.detectors, detector)
+function Control:released(dir)
+  if self._previousValue == 0 or self._currentValue ~= 0 then
+    return false
+  end
+  if dir then
+    if sign(self._previousValue) ~= sign(dir) then
+      return false
+    end
+  end
+  return true
 end
 
---main module
-tactile.__index = tactile
+function Control:update()
+  self._previousValue = self._currentValue
+  self._currentValue = self:getValue()
+end
 
---button detectors
+function tactile.newControl()
+  local control = {
+    deadzone = .5,
+    _detectors = {},
+    _currentValue = 0,
+    _previousValue = 0,
+  }
+
+  setmetatable(control, {__index = Control})
+  return control
+end
+
 function tactile.keys(...)
-  for i = 1, select('#', ...) do
-    verify('tactile.keys', i, select(i, ...), 'string', 'KeyConstant (string)')
-  end
-
   local keys = {...}
   return function()
     return love.keyboard.isDown(unpack(keys))
   end
 end
 
-function tactile.scancodes(...)
-  for i = 1, select('#', ...) do
-    verify('tactile.scancodes', i, select(i, ...), 'string', 'Scancode (string)')
-  end
-
-  local keys = {...}
-  return function()
-    return love.keyboard.isScancodeDown(unpack(keys))
-  end
-end
-
-function tactile.gamepadButtons(gamepadNum, ...)
-  for i = 1, select('#', ...) do
-    verify('tactile.gamepadButtons', i + 1, select(i, ...), 'string', 'GamepadButton (string)')
-  end
-
+function tactile.gamepadButtons(num, ...)
   local buttons = {...}
   return function()
-    local gamepad = type(gamepadNum) == 'number' and love.joystick.getJoysticks()[gamepadNum] or gamepadNum
-    return gamepad and gamepad:isGamepadDown(unpack(buttons))
+    return love.joystick.getJoysticks()[num]:isGamepadDown(unpack(buttons))
   end
 end
 
-function tactile.mouseButtons(...)
-  local major, minor, revision = love.getVersion()
-  local t = "string"
-
-  -- LOVE 0.10+ switched from strings (l, r, m) to numbers (1, 2, 3)
-  if minor > 9 then
-    t = "number"
-  end
-
-  for i = 1, select('#', ...) do
-    verify('tactile.mouseButtons', i, select(i, ...), t, 'MouseButton ('..t..')')
-  end
-
-  local buttons = {...}
+function tactile.gamepadAxis(num, axis)
   return function()
-    return love.mouse.isDown(unpack(buttons))
+    return love.joystick.getJoysticks()[num]:getGamepadAxis(axis)
   end
-end
-
-function tactile.thresholdButton(axisDetector, threshold)
-  verify('tactile.thresholdButton', 1, axisDetector, 'function', 'Axis Detector (function)')
-  verify('tactile.thresholdButton', 2, threshold, 'number', 'a number between -1 and 1')
-
-  return function()
-    local value = axisDetector()
-    return value and math.abs(value) > math.abs(threshold) and (value < 0) == (threshold < 0)
-  end
-end
-
---axis detectors
-function tactile.binaryAxis(negative, positive)
-  verify('tactile.binaryAxis', 1, negative, 'function', 'Axis Detector (function)')
-  verify('tactile.binaryAxis', 2, positive, 'function', 'Axis Detector (function)')
-
-  return function()
-    local negativeValue, positiveValue = negative(), positive()
-    if negativeValue and not positiveValue then
-      return -1
-    elseif positiveValue and not negativeValue then
-      return 1
-    else
-      return 0
-    end
-  end
-end
-
-function tactile.booleanAxis(buttonDetector, whenTrue, whenFalse)
-  verify('tactile.booleanAxis', 1, buttonDetector, 'function', 'Button Detector (function)')
-  verify('tactile.booleanAxis', 2, whenTrue, 'number')
-  verify('tactile.booleanAxis', 3, whenFalse, 'number')
-  return function()
-    return buttonDetector() and whenTrue or whenFalse
-  end
-end
-
-function tactile.gamepadAxis(gamepadNum, axis)
-  verify('tactile.analogStick', 2, axis, 'string', 'GamepadAxis (string)')
-
-  return function()
-    local gamepad = type(gamepadNum) == 'number' and love.joystick.getJoysticks()[gamepadNum] or gamepadNum
-    return gamepad and gamepad:getGamepadAxis(axis) or 0
-  end
-end
-
---button constructor
-function tactile.newButton(...)
-  for i = 1, select('#', ...) do
-    verify('tactile.newButton', i, select(i, ...), 'function', 'Detector (function)')
-  end
-
-  local buttonInstance = {
-    detectors = {...},
-    down      = false,
-    downPrev  = false
-  }
-  return setmetatable(buttonInstance, Button)
-end
-
---axis constructor
-function tactile.newAxis(...)
-  for i = 1, select('#', ...) do
-    verify('tactile.newAxis', i, select(i, ...), 'function', 'Detector (function)')
-  end
-
-  local axisInstance = {
-    detectors = {...},
-    deadzone  = 0.5
-  }
-  return setmetatable(axisInstance, Axis)
 end
 
 return tactile
