33423cc8c2a7cb3dd1aaf6da2d27acbf6de230b1
Andrew Minnich <aminnich3@gmail.com>
2016-03-13 17:22:20 -0400

    More tests

--- test/test.lua
+++ test/test.lua
@@ -11,10 +11,31 @@ local function test_tactile()
       function() return rightKey end
     )
 
+  local xKey = false
+  local fire = tactile.newControl()
+    :addButton(function() return xKey end)
+
   -- test: control value is zero when all detectors are zero
   assert(horizontal:_calculateValue() == 0)
 
   -- test: control obeys deadzone
   leftStick = .4
   assert(horizontal:_calculateValue() == 0)
+
+  -- test: control:getValue() works in general
+  leftStick = -.8
+  assert(horizontal:_calculateValue() == -.8)
+
+  -- test: detectors can override other detectors
+  rightKey = true
+  assert(horizontal:_calculateValue() == 1)
+
+  -- test: negative and positive buttons cancel each other out
+  leftKey = true
+  assert(horizontal:_calculateValue() == -.8)
+
+  -- test: single buttons detectors
+  assert(fire:_calculateValue() == 0)
+  xKey = true
+  assert(fire:_calculateValue() == 1)
 end
