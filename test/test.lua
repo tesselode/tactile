ebc5f84e52e9d0bbb77506bccf9685c57fd862b5
Andrew Minnich <aminnich3@gmail.com>
2016-03-13 17:32:45 -0400

    Add tests for update, isDown, pressed, and released

--- test/test.lua
+++ test/test.lua
@@ -4,6 +4,8 @@ local function test_tactile()
   -- initial setup
   local leftKey, rightKey = false, false
   local leftStick = 0
+  local xKey = false
+
   local horizontal = tactile.newControl()
     :addAxis(function() return leftStick end)
     :addButtonPair(
@@ -11,7 +13,6 @@ local function test_tactile()
       function() return rightKey end
     )
 
-  local xKey = false
   local fire = tactile.newControl()
     :addButton(function() return xKey end)
 
@@ -38,4 +39,35 @@ local function test_tactile()
   assert(fire:_calculateValue() == 0)
   xKey = true
   assert(fire:_calculateValue() == 1)
+
+  -- test: update functionality
+  horizontal:update()
+  assert(horizontal._currentValue == -.8 and horizontal._previousValue == 0)
+  leftStick = 0
+  horizontal:update()
+  assert(horizontal._currentValue == 0 and horizontal._previousValue == -.8)
+
+  -- test: isDown/pressed/released
+  leftStick = 0
+  horizontal:update()
+  assert(not horizontal:isDown())
+  assert(not horizontal:pressed())
+  assert(not horizontal:released())
+
+  leftStick = 1
+  horizontal:update()
+  assert(horizontal:isDown())
+  assert(horizontal:pressed())
+  assert(not horizontal:released())
+
+  horizontal:update()
+  assert(horizontal:isDown())
+  assert(not horizontal:pressed())
+  assert(not horizontal:released())
+
+  leftStick = 0
+  horizontal:update()
+  assert(not horizontal:isDown())
+  assert(not horizontal:pressed())
+  assert(horizontal:released())
 end
